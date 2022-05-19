5ab8781befb139aa52bad66eacfab140eeeea94a
JimLynchCodes <mrdotjim@gmail.com>
2022-05-19 00:55:56 -0400

    fixing apple mint calc

--- packages/hardhat/contracts/TREE.sol
+++ packages/hardhat/contracts/TREE.sol
@@ -35,16 +35,16 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
     mapping(uint256 => TreeData) trees;
 
     // uses tree index
-    uint256[] public trees_for_sale;
+    uint256[] trees_for_sale;
 
     // uses tree index
-    uint256[] public trees_for_breeding;
+    uint256[] trees_for_breeding;
 
     struct TreeData {
         bool isForSale; // 1 byte
         bool isListedForBreeding; // 1 byte
         uint8 growthStrength; // 1 byte    // min value: 1, max value: 10
-        string trunk_color; // 1 byte per char
+        string trunk_color;        // 1 byte per char
         string leaf_primary_color; // 1 byte per char
         string leaf_secondary_color; // 1 byte per char
         uint256 tokenId; // 32 bytes
@@ -148,40 +148,30 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
             (trees[mate_tree_token].breedingPrice * breedingCommision) / 100
         );
 
-        uint256 offspring_generation = 1 +
-            (
-                trees[mate_tree_token].gen >= trees[my_tree_token].gen
-                    ? trees[mate_tree_token].gen
-                    : trees[my_tree_token].gen
-            );
+        uint256 offspring_generation = 1 + uint(trees[mate_tree_token].gen >=
+            trees[my_tree_token].gen
+            ? trees[mate_tree_token].gen
+            : trees[my_tree_token].gen);
 
         uint256 offspring_growth_speed = (trees[mate_tree_token].growthSpeed +
             trees[my_tree_token].growthSpeed) / 2;
 
         uint8 offspring_growth_strength = (trees[mate_tree_token]
             .growthStrength + trees[my_tree_token].growthStrength) / 2;
-
+            
         uint256 offspring_sapling_growth_time = (trees[mate_tree_token]
             .sapling_growth_time + trees[my_tree_token].sapling_growth_time) /
             2;
 
         // TODO - find "average" of colors and styles...
-        string memory offspring_trunk_color = TREE_helpers.averageOfColors(
-            trees[mate_tree_token].trunk_color,
-            trees[my_tree_token].trunk_color
-        );
+        string memory offspring_trunk_color = TREE_helpers.averageOfColors(trees[mate_tree_token]
+            .trunk_color, trees[my_tree_token].trunk_color);
 
-        string memory offspring_leaf_primary_color = TREE_helpers
-            .averageOfColors(
-                trees[mate_tree_token].leaf_primary_color,
-                trees[my_tree_token].leaf_primary_color
-            );
+        string memory offspring_leaf_primary_color = TREE_helpers.averageOfColors(trees[mate_tree_token]
+            .leaf_primary_color, trees[my_tree_token].leaf_primary_color);
 
-        string memory offspring_leaf_secondary_color = TREE_helpers
-            .averageOfColors(
-                trees[mate_tree_token].leaf_secondary_color,
-                trees[my_tree_token].leaf_secondary_color
-            );
+        string memory offspring_leaf_secondary_color = TREE_helpers.averageOfColors(trees[mate_tree_token]
+            .leaf_secondary_color, trees[my_tree_token].leaf_secondary_color);
 
         _safeMint(msg.sender, next_tree_token_id);
 
@@ -202,10 +192,12 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
             offspring_growth_speed
         );
 
+        cancel_breeding_listing(mate_tree_token);
+
         next_tree_token_id++;
     }
 
-    function cancel_breeding_listing(uint256 tokenId) external whenNotPaused {
+    function cancel_breeding_listing(uint256 tokenId) public whenNotPaused {
         trees[tokenId].isListedForBreeding = false;
 
         // swap n' pop removal from breeding array
@@ -281,11 +273,13 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
             (trees[tree_token_id].sellingPrice * salesCommision) / 100
         );
 
+        cancel_for_sale(tree_token_id);
+
         // transfer TREE NFT to the buyer
         _transfer(ownerOf(tree_token_id), msg.sender, tree_token_id);
     }
 
-    function cancel_for_sale(uint256 tokenId) external whenNotPaused {
+    function cancel_for_sale(uint256 tokenId) public whenNotPaused {
         // gives approval rights back to the current owner.
         approve(ownerOf(tokenId), tokenId);
 
@@ -352,55 +346,6 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
         APPLE_address = newTreeAddress;
     }
 
-    // function tokenURI(uint256 tokenId)
-    //     public
-    //     view
-    //     override(ERC721)
-    //     returns (string memory)
-    // {
-    //     string memory json = Base64.encode(
-    //         bytes(
-    //             string(
-    //                 abi.encodePacked(
-    //                     '{"name": "',
-    //                     "NAME",
-    //                     '",',
-    //                     '"image_data": "',
-    //                     TREE_helpers.getSvg("brown"),
-    //                     '",',
-    //                     '"attributes": [{"trait_type": "Speed", "value": ',
-    //                     TREE_helpers.uint2str(42),
-    //                     "},",
-    //                     '{"trait_type": "Attack", "value": ',
-    //                     TREE_helpers.uint2str(55),
-    //                     "},",
-    //                     '{"trait_type": "Defence", "value": ',
-    //                     TREE_helpers.uint2str(1000),
-    //                     "},",
-    //                     '{"trait_type": "Growth Strength", "value": ',
-    //                     TREE_helpers.uint2str(trees[tokenId].growthStrength),
-    //                     "},",
-    //                     '{"trait_type": "Growth Speed", "value": ',
-    //                     TREE_helpers.uint2str(trees[tokenId].growthSpeed),
-    //                     "},",
-    //                     '{"trait_type": "Sapling Growth Time", "value": ',
-    //                     TREE_helpers.uint2str(
-    //                         trees[tokenId].sapling_growth_time
-    //                     ),
-    //                     "},",
-    //                     '{"trait_type": "Leaves Color", "value": "',
-    //                     trees[tokenId].leaves_color,
-    //                     '"}',
-    //                     '{"trait_type": "Trunk Color", "value": "',
-    //                     trees[tokenId].trunk_color,
-    //                     '"}',
-    //                     "]}"
-    //                 )
-    //             )
-    //         )
-    //     );
-    //     return string(abi.encodePacked("data:application/json;base64,", json));
-    // }
 
     function tokenURI(uint256 tokenId)
         public
@@ -413,15 +358,10 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
                 string(
                     abi.encodePacked(
                         '{"name": "',
-                        "TREE #",
-                        TREE_helpers.uint2str(tokenId),
+                        'TREE #', TREE_helpers.uint2str(tokenId),
                         '",',
                         '"image_data": "',
-                        TREE_helpers.getSvg(
-                            trees[tokenId].trunk_color,
-                            trees[tokenId].leaf_primary_color,
-                            trees[tokenId].leaf_secondary_color
-                        ),
+                        TREE_helpers.getSvg(trees[tokenId].trunk_color,trees[tokenId].leaf_primary_color,trees[tokenId].leaf_secondary_color),
                         '",',
                         '"description": "A nice little description...",',
                         '"attributes": [',
@@ -432,20 +372,14 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
                         TREE_helpers.uint2str(trees[tokenId].growthSpeed),
                         "},",
                         '{"trait_type": "Sapling Growth Time", "value": ',
-                        TREE_helpers.uint2str(
-                            trees[tokenId].sapling_growth_time
-                        ),
+                        TREE_helpers.uint2str(trees[tokenId].sapling_growth_time),
                         "},",
                         '{"display_type": "date", "trait_type": "Birthday", "value": ',
-                        TREE_helpers.uint2str(
-                            trees[tokenId].birthday_timestamp
-                        ),
-                        "},",
+                        TREE_helpers.uint2str(trees[tokenId].birthday_timestamp),
+                        '},',
                         '{"display_type": "date", "trait_type": "Last Picked APPLE", "value": ',
-                        TREE_helpers.uint2str(
-                            trees[tokenId].last_picked_apple_timestamp
-                        ),
-                        "},",
+                        TREE_helpers.uint2str(trees[tokenId].last_picked_apple_timestamp),
+                        '},',
                         '{"display_type": "number", "trait_type": "Generation", "value": "',
                         TREE_helpers.uint2str(trees[tokenId].gen),
                         '"},',
@@ -457,7 +391,7 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
                         '"},',
                         '{"trait_type": "Leaf Secondary", "value": "',
                         trees[tokenId].leaf_secondary_color,
-                        '"}', // no comma here
+                        '"}',  // no comma here
                         "]}"
                     )
                 )
@@ -466,15 +400,15 @@ contract TREE is ERC721, ERC721Holder, Ownable, Pausable {
         return string(abi.encodePacked("data:application/json;base64,", json));
     }
 
-    // function getTreesForSale() external view returns (uint256[] memory) {
-    //     return trees_for_sale;
-    // }
+    function getTreesForSale() external view returns (uint256[] memory) {
+        return trees_for_sale;
+    }
 
-    // function getTreesListedForBreeding()
-    //     external
-    //     view
-    //     returns (uint256[] memory)
-    // {
-    //     return trees_for_breeding;
-    // }
+    function getTreesListedForBreeding()
+        external
+        view
+        returns (uint256[] memory)
+    {
+        return trees_for_breeding;
+    }
 }
--- packages/hardhat/contracts/TREE_helpers.sol
+++ packages/hardhat/contracts/TREE_helpers.sol
@@ -9,47 +9,18 @@ library TREE_helpers {
         uint256 growthStrength,
         address APPLE_address
     ) public view returns (uint256) {
-        uint256 age_ms = block.timestamp - birthday_timestamp;
+        
+        uint256 age_years = block.timestamp - birthday_timestamp / 60 / 60 / 24 / 365;
 
         return
             growthStrength *
             (10**APPLE(APPLE_address).decimals()) *
-            (age_ms**2 /
-                (age_ms**2 +
-                    APPLE(APPLE_address).get_nutrition_score(msg.sender)));
+            (age_years**2 /
+                (age_years**2 + 
+                    (1 +
+                    1 / APPLE(APPLE_address).get_nutrition_score(msg.sender) / 10**APPLE(APPLE_address).decimals())));
     }
 
-    // handling images on chain
-    // function getSvg(string memory trunk_color)
-    //     internal
-    //     pure
-    //     returns (string memory)
-    // {
-    //     string[3] memory parts;
-
-    //     parts[0] = '<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"   xmlns:svg="http://www.w3.org/2000/svg"   xmlns="http://www.w3.org/2000/svg"   version="1.1"   width="100%"   height="100%"   viewBox="0 0 14 14"   id="svg2">  <metadata     id="metadata8">    <rdf:RDF>      <cc:Work         rdf:about="">        <dc:format>image/svg+xml</dc:format>        <dc:type           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />        <dc:title></dc:title>      </cc:Work>    </rdf:RDF>  </metadata>  <defs     id="defs6" />  <rect     width="14"     height="14"     x="0"     y="0"     id="canvas"     style="fill:none;stroke:none;visibility:hidden" />  <path     d="m 7.75,14 0,-3 L 9,11 C 11.524338,11 12.545311,7.5 10,6.5 11.496636,5.5 11,3 9,2.75 9,2.75 9,1 7,1 5,1 5,2.75 5,2.75 3,3 2.4614171,5.5 4,6.5 1.4966359,7.5 2.4966359,11 5,11 l 1.25,0 0,3 z"     id="tree-deciduous"     style="fill:';
-    //     parts[1] = trunk_color;
-    //     parts[2] = ';fill-opacity:1;stroke:none" /></svg>';
-
-    //     return
-    //         string(
-    //             abi.encodePacked(
-    //                 parts[0],
-    //                 parts[1],
-    //                 parts[2]
-    //             )
-    //         );
-    // }
-
-    // function getSvg(string memory trunk_color) public pure returns (string memory) {
-    //     return string(abi.encodePacked("<svg width='350px' height='350px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'> <path d='M11.55 18.46C11.3516 18.4577 11.1617 18.3789 11.02 18.24L5.32001 12.53C5.19492 12.3935 5.12553 12.2151 5.12553 12.03C5.12553 11.8449 5.19492 11.6665 5.32001 11.53L13.71 3C13.8505 2.85931 14.0412 2.78017 14.24 2.78H19.99C20.1863 2.78 20.3745 2.85796 20.5133 2.99674C20.652 3.13552 20.73 3.32374 20.73 3.52L20.8 9.2C20.8003 9.40188 20.7213 9.5958 20.58 9.74L12.07 18.25C11.9282 18.3812 11.7432 18.4559 11.55 18.46ZM6.90001 12L11.55 16.64L19.3 8.89L19.25 4.27H14.56L6.90001 12Z' fill='red'/> <path d='M14.35 21.25C14.2512 21.2522 14.153 21.2338 14.0618 21.1959C13.9705 21.158 13.8882 21.1015 13.82 21.03L2.52 9.73999C2.38752 9.59782 2.3154 9.40977 2.31883 9.21547C2.32226 9.02117 2.40097 8.83578 2.53838 8.69837C2.67579 8.56096 2.86118 8.48224 3.05548 8.47882C3.24978 8.47539 3.43783 8.54751 3.58 8.67999L14.88 20C15.0205 20.1406 15.0993 20.3312 15.0993 20.53C15.0993 20.7287 15.0205 20.9194 14.88 21.06C14.7353 21.1907 14.5448 21.259 14.35 21.25Z' fill='red'/> <path d='M6.5 21.19C6.31632 21.1867 6.13951 21.1195 6 21L2.55 17.55C2.47884 17.4774 2.42276 17.3914 2.385 17.297C2.34724 17.2026 2.32855 17.1017 2.33 17C2.33 16.59 2.33 16.58 6.45 12.58C6.59063 12.4395 6.78125 12.3607 6.98 12.3607C7.17876 12.3607 7.36938 12.4395 7.51 12.58C7.65046 12.7206 7.72934 12.9112 7.72934 13.11C7.72934 13.3087 7.65046 13.4994 7.51 13.64C6.22001 14.91 4.82 16.29 4.12 17L6.5 19.38L9.86 16C9.92895 15.9292 10.0114 15.873 10.1024 15.8346C10.1934 15.7962 10.2912 15.7764 10.39 15.7764C10.4888 15.7764 10.5866 15.7962 10.6776 15.8346C10.7686 15.873 10.8511 15.9292 10.92 16C11.0605 16.1406 11.1393 16.3312 11.1393 16.53C11.1393 16.7287 11.0605 16.9194 10.92 17.06L7 21C6.8614 21.121 6.68402 21.1884 6.5 21.19Z' fill='", trunk_color,"'/> </svg>"));
-
-    //     // return string(
-    //     //     abi.encodePacked(
-    //     //         "<svg width='350px' height='350px' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'> <path d='M11.55 18.46C11.3516 18.4577 11.1617 18.3789 11.02 18.24L5.32001 12.53C5.19492 12.3935 5.12553 12.2151 5.12553 12.03C5.12553 11.8449 5.19492 11.6665 5.32001 11.53L13.71 3C13.8505 2.85931 14.0412 2.78017 14.24 2.78H19.99C20.1863 2.78 20.3745 2.85796 20.5133 2.99674C20.652 3.13552 20.73 3.32374 20.73 3.52L20.8 9.2C20.8003 9.40188 20.7213 9.5958 20.58 9.74L12.07 18.25C11.9282 18.3812 11.7432 18.4559 11.55 18.46ZM6.90001 12L11.55 16.64L19.3 8.89L19.25 4.27H14.56L6.90001 12Z' fill='red'/> <path d='M14.35 21.25C14.2512 21.2522 14.153 21.2338 14.0618 21.1959C13.9705 21.158 13.8882 21.1015 13.82 21.03L2.52 9.73999C2.38752 9.59782 2.3154 9.40977 2.31883 9.21547C2.32226 9.02117 2.40097 8.83578 2.53838 8.69837C2.67579 8.56096 2.86118 8.48224 3.05548 8.47882C3.24978 8.47539 3.43783 8.54751 3.58 8.67999L14.88 20C15.0205 20.1406 15.0993 20.3312 15.0993 20.53C15.0993 20.7287 15.0205 20.9194 14.88 21.06C14.7353 21.1907 14.5448 21.259 14.35 21.25Z' fill='red'/> <path d='M6.5 21.19C6.31632 21.1867 6.13951 21.1195 6 21L2.55 17.55C2.47884 17.4774 2.42276 17.3914 2.385 17.297C2.34724 17.2026 2.32855 17.1017 2.33 17C2.33 16.59 2.33 16.58 6.45 12.58C6.59063 12.4395 6.78125 12.3607 6.98 12.3607C7.17876 12.3607 7.36938 12.4395 7.51 12.58C7.65046 12.7206 7.72934 12.9112 7.72934 13.11C7.72934 13.3087 7.65046 13.4994 7.51 13.64C6.22001 14.91 4.82 16.29 4.12 17L6.5 19.38L9.86 16C9.92895 15.9292 10.0114 15.873 10.1024 15.8346C10.1934 15.7962 10.2912 15.7764 10.39 15.7764C10.4888 15.7764 10.5866 15.7962 10.6776 15.8346C10.7686 15.873 10.8511 15.9292 10.92 16C11.0605 16.1406 11.1393 16.3312 11.1393 16.53C11.1393 16.7287 11.0605 16.9194 10.92 17.06L7 21C6.8614 21.121 6.68402 21.1884 6.5 21.19Z' fill='red'/> </svg>"
-    //     //     )
-    //     // );
-    // }
 
     function getSvg(
         string memory trunk_color,
@@ -124,4 +95,46 @@ library TREE_helpers {
         // TODO
         return colorB;
     }
+
+    function uint2hexstr(uint i) public pure returns (string memory) {
+        if (i == 0) return "0";
+        uint j = i;
+        uint length;
+        while (j != 0) {
+            length++;
+            j = j >> 4;
+        }
+        uint mask = 15;
+        bytes memory bstr = new bytes(length);
+        uint k = length;
+        while (i != 0) {
+            uint curr = (i & mask);
+            bstr[--k] = curr > 9 ?
+                bytes1(uint8(55 + curr)) :
+                bytes1(uint8(48 + curr)); // 55 = 65 - 10
+            i = i >> 4;
+        }
+        return string(bstr);
+    }
+
+    function hexColorToBytes(string memory _hex) public pure returns (string[3] memory) {
+        // uint8[] memory result;
+        // for (uint8 i = 0; i < 3; i += 2) {
+        //     result[i / 2] = stringToUint(_hex[i+1] + _hex[i + 3], 16);
+        // }
+        // return result;
+
+        return [substring(_hex,1,3),substring(_hex,3,5),substring(_hex,5,7)];
+    }
+
+    function substring(string memory str, uint startIndex, uint endIndex) public pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        bytes memory result = new bytes(endIndex-startIndex);
+        for(uint i = startIndex; i < endIndex; i++) {
+            result[i-startIndex] = strBytes[i];
+        }
+        return string(result);
+    }
+
+
 }
